a7563fc2f8c307bb6ac0d3ad544350bf062f7ce1
gerardo107 <gerchez93@gmail.com>
2018-06-21 19:05:16 -0500

    login layout

---
+++ app/src/main/java/com/example/gerardogarcias/myapplication/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.gerardogarcias.myapplication;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
+}
