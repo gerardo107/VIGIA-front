799abba0987149c61b5d6f0443d378ed63cc96b9
gerardo107 <gerchez93@gmail.com>
2018-06-25 14:38:44 -0500

    boton entrar a layout de registro corregido

--- app/src/main/java/com/example/gerardogarcias/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/gerardogarcias/myapplication/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.gerardogarcias.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,8 +17,31 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        goToMain();
+        goToRegistro();
 
 
+    }
+    private void  goToMain(){
+        CardView MainButton = (CardView) findViewById(R.id.mainButton);
+        MainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void  goToRegistro(){
+
+        TextView RegisterButton = (TextView) findViewById(R.id.botonRegistro);
+
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
     }
 
 }
