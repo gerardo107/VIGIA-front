aa003377a6a51d4b6fdf175c1fae46fcb35fb47b
gerardo107 <gerchez93@gmail.com>
2018-07-04 12:56:36 -0500

    api running in android

--- app/src/main/java/com/example/gerardogarcias/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gerardogarcias/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gerardogarcias.myapplication;
 
+import android.app.VoiceInteractor;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -8,9 +10,21 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     /**
@@ -27,6 +41,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String URL="http://10.0.2.2:3000/requests";
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest =new JsonObjectRequest(
+        Request.Method.GET,
+        URL,
+        null,
+        new Response.Listener<JSONObject>(){
+            @Override
+            public void onResponse (JSONObject response){
+                Log.e("Rest Response",response.toString());
+            }
+        },
+        new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Log.e("Rest Response",error.toString());
+            }
+        }
+        );
+
+        requestQueue.add(objectRequest);
 
         setToolbar(); // Setear Toolbar como action bar
 
@@ -85,9 +123,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
