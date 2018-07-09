0812b066ce92d87999ca612ac303f066dfea56e2
gerardo107 <gerchez93@gmail.com>
2018-07-09 12:18:01 -0500

    showing the data of the api in buttons

--- app/src/main/java/com/example/gerardogarcias/myapplication/PlaceholderFragment.java
+++ app/src/main/java/com/example/gerardogarcias/myapplication/PlaceholderFragment.java
@@ -13,9 +13,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
+import com.google.gson.JsonArray;
+import android.support.v7.widget.CardView;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -23,39 +27,72 @@ import org.json.JSONObject;
  */
 public class PlaceholderFragment extends Fragment {
 
+    TextView mainMenuText;
+    CardView mainMenuButtons;
+    RequestQueue requestQueue;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.section_fragment, container, false);
 
-        String URL="http://10.0.2.2:3000/requests";
 
-        RequestQueue requestQueue=  Volley.newRequestQueue(getActivity().getApplicationContext());
 
-        JsonObjectRequest objectRequest =new JsonObjectRequest(
-                Request.Method.GET,
-                URL,
-                null,
-                new Response.Listener<JSONObject>(){
-                    @Override
-                    public void onResponse (JSONObject response){
-                        Log.e("Rest Response",response.toString());
-                    }
-                },
-                new Response.ErrorListener(){
-                    @Override
-                    public void onErrorResponse(VolleyError error){
-                        Log.e("Rest Response",error.toString());
-                    }
-                }
-        );
+        mainMenuText = view.findViewById(R.id.mainMenuText);
+        mainMenuButtons = view.findViewById(R.id.mainMenuButtons);
+        requestQueue =  Volley.newRequestQueue(getActivity().getApplicationContext());
 
-        requestQueue.add(objectRequest);
+
+        jsonParese();
+
+
+
+
+
+
+
 
 
 
         return view;
     }
 
+    private void jsonParese(){
+
+        String url="http://10.0.2.2:3000/requests";
+
+
+
+        JsonArrayRequest request =new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>(){
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+
+
+
+                            for(int i =0; i < response.length(); i++){
+                                JSONObject requests = response.getJSONObject(i);
+
+                                String name = requests.getString("name" );
+
+                                mainMenuText.append(name + "\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        error.printStackTrace();
+                    }
+                }
+        );
+        requestQueue.add(request);
+
+
+    }
 }
--- app/src/main/java/com/example/gerardogarcias/myapplication/ReportesFragment.java
+++ app/src/main/java/com/example/gerardogarcias/myapplication/ReportesFragment.java
@@ -8,6 +8,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class ReportesFragment extends Fragment {
 
@@ -17,21 +31,29 @@ public class ReportesFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.reportes_fragment, container, false);
 
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Fragment Inbox");
+        String URL="http://10.0.2.2:3000/requests";
 
-        Button buttonChangeText = (Button) view.findViewById(R.id.buttonFragmentInbox);
+        RequestQueue requestQueue= Volley.newRequestQueue(getActivity().getApplicationContext());
 
-        final TextView textViewInboxFragment = (TextView) view.findViewById(R.id.textViewInboxFragment);
+        JsonArrayRequest getRequest =new JsonArrayRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONArray>(){
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.e("Rest Response",response.toString());
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        Log.e("Rest Response",error.toString());
+                    }
+                }
+        );
 
-        buttonChangeText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                textViewInboxFragment.setText("This is the Inbox Fragmentss");
-
-
-            }
-        });
+        requestQueue.add(getRequest);
 
         return view;
     }
