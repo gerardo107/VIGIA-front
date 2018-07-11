6a5dc1c3c308b7331336da3055e40ab9e5f51762
gerardo107 <gerchez93@gmail.com>
2018-07-11 11:45:07 -0500

    main menu terminado

--- app/src/main/java/com/example/gerardogarcias/myapplication/PlaceholderFragment.java
+++ app/src/main/java/com/example/gerardogarcias/myapplication/PlaceholderFragment.java
@@ -1,36 +1,45 @@
 package com.example.gerardogarcias.myapplication;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonArray;
 import android.support.v7.widget.CardView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 /**
  * Fragmento para el contenido principal
  */
 public class PlaceholderFragment extends Fragment {
 
+    LinearLayout parent;
     TextView mainMenuText;
     CardView mainMenuButtons;
     RequestQueue requestQueue;
+    int valueDP_Width, Value_In_Pixel_Width;
+    int valueDP_Height,Value_In_Pixel_Height;
+    int valueDP_Radius,Value_In_Pixel_Radius;
+    int valueDP_Elevation,Value_In_Pixel_Elevation;
 
+    Button myButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -39,30 +48,21 @@ public class PlaceholderFragment extends Fragment {
 
 
 
-        mainMenuText = view.findViewById(R.id.mainMenuText);
-        mainMenuButtons = view.findViewById(R.id.mainMenuButtons);
+
+
+
         requestQueue =  Volley.newRequestQueue(getActivity().getApplicationContext());
+        parent=view.findViewById(R.id.parentLayout);
 
 
         jsonParese();
-
-
-
-
-
-
-
-
-
 
         return view;
     }
 
     private void jsonParese(){
-
+        //URL de la api del primer menu
         String url="http://10.0.2.2:3000/requests";
-
-
 
         JsonArrayRequest request =new JsonArrayRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONArray>(){
@@ -71,13 +71,57 @@ public class PlaceholderFragment extends Fragment {
                         try {
 
 
-
                             for(int i =0; i < response.length(); i++){
+
                                 JSONObject requests = response.getJSONObject(i);
 
                                 String name = requests.getString("name" );
 
-                                mainMenuText.append(name + "\n");
+                                //cambiando de valores de dp a px para el Width de los botones
+                                valueDP_Width = 300;//value in dp
+                                Value_In_Pixel_Width = (int) TypedValue.applyDimension(
+                                        TypedValue.COMPLEX_UNIT_DIP, valueDP_Width, getResources().getDisplayMetrics());
+
+                                //cambiando de valores de dp a px para el Height de los botones
+                                valueDP_Height = 50;//value in dp
+                                Value_In_Pixel_Height = (int) TypedValue.applyDimension(
+                                        TypedValue.COMPLEX_UNIT_DIP, valueDP_Height, getResources().getDisplayMetrics());
+
+                                //cambio de valores de dp a px para radius
+                                valueDP_Radius = 25;//value in dp
+                                Value_In_Pixel_Radius = (int) TypedValue.applyDimension(
+                                        TypedValue.COMPLEX_UNIT_DIP, valueDP_Radius, getResources().getDisplayMetrics());
+
+                                //cambio de valores de dp a px para CardElevation
+                                valueDP_Elevation = 10;//value in dp
+                                Value_In_Pixel_Elevation = (int) TypedValue.applyDimension(
+                                        TypedValue.COMPLEX_UNIT_DIP, valueDP_Elevation, getResources().getDisplayMetrics());
+
+                                //caracteristicas de los botones
+                                mainMenuButtons = new CardView(getActivity().getApplicationContext());
+                                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(Value_In_Pixel_Width,Value_In_Pixel_Height);
+                                params.setMargins(0,0,0,210);
+                                params.gravity = Gravity.CENTER;
+                                mainMenuButtons.setLayoutParams(params);
+                                mainMenuButtons.setRadius(Value_In_Pixel_Radius);
+                                mainMenuButtons.setCardBackgroundColor(Color.parseColor("#FF4081"));
+                                mainMenuButtons.setCardElevation(Value_In_Pixel_Elevation);
+
+                                //caracteristicas del texto
+                                mainMenuText =new TextView(getActivity().getApplicationContext());
+                                mainMenuText.setText(name);
+                                mainMenuText.setGravity(Gravity.CENTER);
+                                mainMenuText.setTextColor(Color.parseColor("#FFFFFF"));
+                                mainMenuText.setTextSize(18);
+
+
+                                //agregar el texto a los botones
+                                mainMenuButtons.addView(mainMenuText);
+
+                                //agregar los botones al layout
+                                parent.addView(mainMenuButtons);
+
+
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
